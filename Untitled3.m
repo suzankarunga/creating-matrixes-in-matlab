3788bf095d9d714c4fe7e153572d4caddb62ce8f
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2022-10-26 05:00:22 -0900

    Add files via upload

---
+++ Untitled3.m
@@ -0,0 +1,28 @@
+clear all;
+close all;
+
+% creating a row vector
+x=[1 2 3 4 5];
+
+
+% creating a column vector
+
+y=[6;7;8;9;10];
+y
+y=[6 7 8 9 10];
+
+
+u=[0:8];
+u
+
+t=[1:0.5:10];
+t
+
+v=[0:2:8]; 
+v
+v(1:3);
+v(1:2:4);
+
+
+
+
