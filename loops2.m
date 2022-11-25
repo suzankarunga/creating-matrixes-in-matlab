d0a5f060eecb0e88ae7e07bfd2df7fcb9c05542c
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2022-11-24 21:25:27 -0900

    Add files via upload

---
+++ loops2.m
@@ -0,0 +1,11 @@
+clear all ;
+close all;
+Balance = 5000; %initialize the balance 
+year =0; %initialize year counter
+while   Balance < 1000000
+    Balance = (1.09)*Balance +5000;
+    year = year + 1
+    
+    
+end
+year ,Balance
